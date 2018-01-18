6de2b6b767c137caf31e1e7c363ac1e8eb0d1add
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-18 14:56:44 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,27 @@
 require 'pry'
 
 class Game
+attr_accessor :player_1, :player_2, :board
 
-  include Players
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-  WIN_COMBINATIONS = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[6,4,2]
-  ]
-
-  attr_accessor :board, :player_1, :player_2
-
-  def initialize(player_1 = Human.new("X"), player_2 = Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
   end
 
   def current_player
-    if @board.turn_count.even?
-      @player_1
-    elsif @board.turn_count.odd?
-      @player_2
-    end
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2
   end
 
   def over?
@@ -31,96 +29,101 @@ class Game
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      w_1 = combo[0]
-      w_2 = combo[1]
-      w_3 = combo[2]
-
-      p_1 = @board.cells[w_1]
-      p_2 = @board.cells[w_2]
-      p_3 = @board.cells[w_3]
-
-      p_1 == p_2 && p_2 == p_3 && @board.taken?(w_1 + 1)
+    WIN_COMBINATIONS.detect do |el|
+      @board.cells[el[0]] == @board.cells[el[1]] &&
+      @board.cells[el[1]] == @board.cells[el[2]] &&
+      @board.taken?(el[0]1)
+      # binding.pry
     end
   end
 
+  # def won?  # <<< --- WHY DOES THIS NOT WORK WITH "O"?
+  #   winning_combo = []
+  #   WIN_COMBINATIONS.each do | el |
+  #     if el.all? {|i| board.cells[i] == "O"}
+  #         # binding.pry
+  #         winning_combo << el
+  #         return winning_combo.flatten
+  #     elsif el.all? {|i| board.cells[i] == "X"}
+  #         # binding.pry
+  #         winning_combo << el
+  #         return winning_combo.flatten
+  #       end
+  #     end
+  #   !winning_combo.empty? && over?
+  # end
+
   def draw?
-    if won?
-      false
-    elsif @board.full? && !won?
-      true
-    end
+    !won? && @board.full?
   end
 
   def winner
-    #binding.pry
-    @board.cells[won?[0]] if won?
+    won? ? @board.cells[won?[0]] : nil
   end
 
   def turn
-    @board.display
-    puts "Please enter 1 - 9"
-    player = current_player
-    move = player.move(board)
-    if @board.valid_move?(move)
-      @board.update(move, player)
-      #@board.display
-    else
-      puts "Invalid move."
+    puts "Player '#{current_player.token}' - Please select a square: "
+    input = current_player.move(@board)
+    if !@board.valid_move?(input)
+      puts "invalid"
       turn
+    else
+      @board.update(input, current_player)
+      puts @board.display
     end
-    #binding.pry
   end
 
   def play
     while !over?
       turn
     end
+
     if won?
       puts "Congratulations #{winner}!"
-      @board.display
-      puts "Would you like to play again? (y/n)"
-      answer = gets
-      if answer == "y"
-        Game.start
-      end
+      self.play_again?
     elsif draw?
       puts "Cat's Game!"
-      @board.display
-      puts "Would you like to play again? (y/n)"
-      answer = gets.chomp
-      if answer == "y"
-        Game.start
-      end
+      self.play_again?
     end
   end
 
-  def self.start
-    puts "Welcome to Tic Tac Toe!"
-    puts "Would you like to play 0, 1 or 2 player?"
-    input = gets.chomp
-    #binding.pry
-    if input == "0"
-      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
-
-      game.play
-    elsif input == "1"
-     puts "Who should go first and be X?"
-     puts "Enter 1 to be player 1, or 2 to be player 2."
-     input_2 = gets.chomp
-     if input_2 == "1"
-       game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
-       game.play
-     elsif input_2 == "2"
-       game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
-       game.play
-     end
-    elsif input == 2
-     Game.new.play
+  def play_again?
+    puts "Play again? (Y/N)"
+    input = gets.chomp.downcase
+    if input == "y"
+      start
     else
-     puts "Invalid entry"
-     start
+      puts "Thanks!"
+      exit
+    end
+  end
+
+  def start
+  puts "What kind of game do you want? 0, 1, or 2 player."
+  # Ask the user for who should go first and be "X".
+
+    input = gets.chomp
+
+    if input == "0"
+      current_game = Game.new(Players::Computer.new("X"), Players::Computer.new("0"))
+    elsif input == "1"
+      puts "Who goes first as 'X'? (C)omputer or (H)uman?"
+      human_or_comp = gets.chomp.downcase
+      if human_or_comp == "h"
+        current_game = Game.new(Players::Human.new("X"), Players::Computer.new("0"))
+      elsif human_or_comp == "c"
+        current_game = Game.new(Players::Computer.new("X"), Players::Human.new("0"))
+      end
+    elsif input == "2"
+      current_game = Game.new
+    elsif input.downcase == "exit"
+      exit
+    else
+      puts "Please enter a valid game type."
     end
 
+    current_game.board.display
+
+    current_game.play
   end
 end
