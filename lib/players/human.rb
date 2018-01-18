9cecf52a2699be4564826dadc8a6712957a33b0f
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-18 14:50:42 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -91,21 +91,21 @@ module Players
             corners.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             edges.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             return "9"
-          elsif p_1 == opp && p_2 == opp && board.valid_move?((w_3  1).to_s)                 #block
+          elsif p_1 == opp && p_2 == opp && board.valid_move?((w_3 + 1).to_s)                 #block
             moves.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             corners.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             edges.delete_if {|m| board.position(m) == opp || board.position(m) == token}
-            return (w_3  1).to_s #if 1st 2 win combos are opp play 3rd
-          elsif p_1 == opp && p_3 == opp && board.valid_move?((w_2  1).to_s)               #block
+            return (w_3 + 1).to_s #if 1st 2 win combos are opp play 3rd
+          elsif p_1 == opp && p_3 == opp && board.valid_move?((w_2 + 1).to_s)               #block
             moves.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             corners.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             edges.delete_if {|m| board.position(m) == opp || board.position(m) == token}
-            return (w_2  1).to_s #if 1st and 3rd win combos are opp play 2nd
-          elsif p_2 == opp && p_3 == opp && board.valid_move?((w_1  1).to_s)               #block
+            return (w_2 + 1).to_s #if 1st and 3rd win combos are opp play 2nd
+          elsif p_2 == opp && p_3 == opp && board.valid_move?((w_1 + 1).to_s)               #block
             moves.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             corners.delete_if {|m| board.position(m) == opp || board.position(m) == token}
             edges.delete_if {|m| board.position(m) == opp || board.position(m) == token}
-            return (w_1  1).to_s #if 2nd and 3rd win combos are opp play 1st
+            return (w_1 + 1).to_s #if 2nd and 3rd win combos are opp play 1st
           end #small if
         end #detect
 
